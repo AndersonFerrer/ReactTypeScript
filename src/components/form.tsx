381479c1bb8ee46ec9fc9934a3145dcd309d9362
AndersonFerrer <ferrer.280104@gmail.com>
2023-09-25 21:22:29 -0500

    v1.0.0

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,8 +1,9 @@
-import { Formik, Field, Form, FormikHelpers } from 'formik';
+import { Formik, Field, Form, FormikHelpers, ErrorMessage } from 'formik';
 import { DetailUser } from '../interfaces/interface'
 import '../App.css'
 import { useState } from 'react';
 import { Modal } from 'antd';
+import * as Yup from 'yup'
 
 
 interface Props {
@@ -12,8 +13,6 @@ interface Props {
     modalView: boolean,
     setModalView: React.Dispatch<React.SetStateAction<boolean>>
 }
-/* const ReachableContext = createContext<string | null>(null);
-const UnreachableContext = createContext<string | null>(null); */
 const config = {
     title: 'Se añadio Nuevo Usuario',
 };
@@ -22,11 +21,34 @@ const config = {
 export default function MyApp(props: Props) {
     const [modal, contextHolder] = Modal.useModal();
     const [newId, setNewId] = useState(props.indexId)
+    const validationSchema = Yup.object({
+        name: Yup.string().required('El nombre es requerido'),
+        username: Yup.string().required('El nombre de usuario es requerido'),
+        email: Yup.string().email('Correo electrónico no válido')
+            .required('El correo electrónico es obligatorio').test('email-exists', 'Este correo ya está registrado', async function (value) {
+                // Consulta la lista de usuarios en localStorage para verificar si el correo ya existe
+                const users = JSON.parse(localStorage.getItem('users') || '[]');
+                console.log(!users.some((user: DetailUser) => user.email === value))
+                return !users.some((user: DetailUser) => user.email === value);
+            }),
+        address: Yup.object({
+            street: Yup.string().required('La calle es requerida'),
+            suite: Yup.string().required('La suite es requerida'),
+            city: Yup.string().required('La ciudad es requerida'),
+            zipcode: Yup.string().matches(/^[0-9]+$/, 'Ingresar solo numeros').required('El zipcode es requerido'),
+        }),
+        phone: Yup.string().matches(/^[0-9]+$/, 'Ingresar solo numeros').required('El celular es requerido'),
+        website: Yup.string().required('El sitio web es requerido'),
+        company: Yup.object({
+            name: Yup.string().required('La compañia es requerida'),
+        })
+    })
 
     const handleSubmit = (
         values: DetailUser,
-        { setSubmitting }: FormikHelpers<DetailUser>
+        { setSubmitting, resetForm }: FormikHelpers<DetailUser>
     ) => {
+
         const id = newId + 1
         values.id = id
         console.log(id)
@@ -51,12 +73,15 @@ export default function MyApp(props: Props) {
         setSubmitting(false)
         setNewId(id)
         props.setModalView(false)
+        resetForm()
+
     }
     console.log(props.modalView)
 
     return (
         <div className={`form__modal ${props.modalView ? 'form__modal__view' : ''}`}>
             <Formik
+                validationSchema={validationSchema}
                 initialValues={{
                     id: newId + 1,
                     name: '',
@@ -90,6 +115,7 @@ export default function MyApp(props: Props) {
                             name="name"
                             placeholder="John"
                         />
+                        <ErrorMessage className='text-danger' name="name" component="div" />
                     </div>
                     <div className='form__div'>
                         <label htmlFor="lastName">Nombre de Usuario</label>
@@ -99,6 +125,7 @@ export default function MyApp(props: Props) {
                             name="username"
                             placeholder="Doe"
                         />
+                        <ErrorMessage className='text-danger' name="username" component="div" />
                     </div>
                     <div className='form__div'>
                         <label htmlFor="email">Email</label>
@@ -109,6 +136,7 @@ export default function MyApp(props: Props) {
                             placeholder="john@acme.com"
                             type="email"
                         />
+                        <ErrorMessage className='text-danger' name="email" component="div" />
                     </div>
                     <div className='form__div'>
                         <label htmlFor="email">Direccion</label>
@@ -117,28 +145,32 @@ export default function MyApp(props: Props) {
                                 className="form__input"
                                 id="address.street"
                                 name="address.street"
-                                placeholder="calle"
+                                placeholder="Calle"
                             />
+                            <ErrorMessage className='text-danger' name="address.street" component="div" />
                             <Field
                                 className="form__input"
                                 id="address.suite"
                                 name="address.suite"
-                                placeholder="suite"
+                                placeholder="Suite"
                             />
+                            <ErrorMessage className='text-danger' name="address.suite" component="div" />
                         </div>
                         <div id='form__address'>
                             <Field
                                 className="form__input"
                                 id="address.zipcode"
                                 name="address.zipcode"
-                                placeholder="zipcode"
+                                placeholder="Codigo Postal"
                             />
+                            <ErrorMessage className='text-danger' name="address.zipcode" component="div" />
                             <Field
                                 className="form__input"
                                 id="address.city"
                                 name="address.city"
-                                placeholder="city"
+                                placeholder="Ciudad"
                             />
+                            <ErrorMessage className='text-danger' name="address.city" component="div" />
                         </div>
                     </div>
                     <div className='form__div'>
@@ -147,8 +179,9 @@ export default function MyApp(props: Props) {
                             className="form__input"
                             id="phone"
                             name="phone"
-                            placeholder="phone"
+                            placeholder="Celular"
                         />
+                        <ErrorMessage className='text-danger' name="phone" component="div" />
                     </div>
                     <div className='form__div'>
                         <label htmlFor="firstName">Sitio Web</label>
@@ -156,8 +189,9 @@ export default function MyApp(props: Props) {
                             className="form__input"
                             id="website"
                             name="website"
-                            placeholder="sitio web"
+                            placeholder="Sitio Web"
                         />
+                        <ErrorMessage className='text-danger' name="website" component="div" />
                     </div>
                     <div className='form__div'>
                         <label htmlFor="firstName">Compañia</label>
@@ -165,8 +199,9 @@ export default function MyApp(props: Props) {
                             className="form__input"
                             id="company.name"
                             name="company.name"
-                            placeholder="compañia"
+                            placeholder="Nombre de compañia"
                         />
+                        <ErrorMessage className='text-danger' name="company.name" component="div" />
                     </div>
                     <button className='form__btn' type="submit">Agregar</button>
                 </Form>
--- src/pages/comments.tsx
+++ src/pages/comments.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../index.css';
+import { Skeleton } from 'antd';
 
 interface CommentsType {
     postId: number,
@@ -15,6 +16,7 @@ const Comments: React.FC = () => {
     const [currentPage, setCurrentPage] = useState(0)
     const commentsPerPage = 6
     const pages = []
+    const [loader, setLoader] = useState(true)
 
     for (let i = 0; i < data.length; i += commentsPerPage) {
         const page = data.slice(i, i + commentsPerPage);
@@ -22,30 +24,41 @@ const Comments: React.FC = () => {
     }
 
     const currentComments = pages[currentPage];
+    const localData = localStorage.getItem('comments')
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/comments')
-            .then((res) => res.json())
-            .then((res) => {
-                setData(res);
-            });
+        if (localData) {
+            setData(JSON.parse(localData))
+            setLoader(false)
+        } else {
+            fetch('https://jsonplaceholder.typicode.com/comments')
+                .then((res) => res.json())
+                .then((res) => {
+                    localStorage.setItem('comments', JSON.stringify(res))
+                    setData(res)
+                    setTimeout(() => {
+                        setLoader(false)
+                    }, 1000);
+                })
+                .catch((error) => {
+                    console.error(error)
+                });
+        }
     }, []);
-    /*   console.log(data)
-      console.log(pages.length) */
 
     return (
         <section className='container__childrens'>
             <div className='container__list'>
                 <h1>Comentarios</h1>
-
-                {currentComments?.map((comment) => (
+                {loader ? currentComments?.map((comment) => (
+                    <Skeleton key={comment.id} active={loader} />
+                )) : currentComments?.map((comment) => (
                     <div className='list__comment' key={comment.id}>
                         <h2 className='list__commet__title'>{comment.name}</h2>
                         <p className='list__comment__subtitle'>{comment.email}</p>
                         <p className='list__comment__subtitle'>{comment.body}</p>
                     </div>
                 ))}
-
                 <div className='list__buttons'>
                     <button
                         className='list__button'
